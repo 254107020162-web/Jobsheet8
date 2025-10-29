e206d80ae5de47f9e7e0658c9aad47eaa0bd2eb6
254107020162-web <254107020162@student.polinema.ac.id>
2025-10-29 12:19:22 +0700

    NilaiKelompok26

---
+++ NilaiKelompok26.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class NilaiKelompok26 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int i =1, nilai;
+        double ratanilai, totalnilai;
+
+        while (i <= 6) {
+            System.out.println("kelompok " + i);
+            totalnilai = 0;
+            for (int j =1; j <= 5; j++) {
+                System.out.print("nilai dari kelompok penilai " + j + " : ");
+                nilai = scanner.nextInt();
+                totalnilai += nilai;
+            }
+            ratanilai = totalnilai / 5;
+            System.out.println("rata-rata nilai kelompok " + i + " : " + ratanilai);
+            i++;
+        }
+    }
+}
